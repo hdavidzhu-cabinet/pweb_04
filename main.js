cdc32c16708603b258acfbb6485406de4b009824
David <huanzhendavidzhu@gmail.com>
2013-11-02 10:53:33 -0400

    Fade in effect.

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ $(document).ready(function(){
 	$(".sidelink").click(function(){
 		var page = $(this).attr("id");
 		$.get("/contact",function(res){
-			$(".mainblock").html(res);
+			// $(".mainblock").html(res);
+			$(".mainblock").fadeIn(400,function(res){
+				$(this).html(res);
+			});
 		});
 		return false;
 	});
